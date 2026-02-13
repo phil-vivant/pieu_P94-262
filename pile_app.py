9a9eed361a56e669b6334a38b5f5860cc0e85e39
phil-vivant <philippe.vivant@gmail.com>
2026-02-13 10:51:40 +0100

    typo corrected

--- pile_app.py
+++ pile_app.py
@@ -24,7 +24,7 @@ APP_STATE_KEYS = [
     "soil_b_name", "soil_b_zsup", "soil_b_zinf", "soil_b_curve", "soil_b_pf", "soil_b_pl", "soil_b_Em", "soil_b_alpha", "soil_b_type"
 
     # Sol C
-    "soil_c_name", "soil_c_zsup", "soil_c_zinf", "soil_c_curve", "soil_c_pf", "soil_c_pl", "soil_c_Em", "soil_c_alpha",, "soil_c_type"
+    "soil_c_name", "soil_c_zsup", "soil_c_zinf", "soil_c_curve", "soil_c_pf", "soil_c_pl", "soil_c_Em", "soil_c_alpha", "soil_c_type"
 
     # Sol D
     "soil_d_name", "soil_d_zsup", "soil_d_zinf", "soil_d_curve", "soil_d_pf", "soil_d_pl", "soil_d_Em", "soil_d_alpha", "soil_d_type"
