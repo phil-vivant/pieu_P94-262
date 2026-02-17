cf44ff2f162eff4ae3c6bb08dc356c0304265b01
Philippe VIVANT <philippe.vivant@gmail.com>
2026-02-17 21:42:58 +0100

    Update pile_app.py

--- pile_app.py
+++ pile_app.py
@@ -515,8 +515,8 @@ if tog_equ == True:
             )
         )
         fig1.layout.title.text = "Courbe de tassement"
-        fig1.layout.xaxis.title = "Charge vertical en tête de pieu [kN]"
-        fig1.layout.yaxis.title = "Déplacement vertical en tête de pieu [mm]"
+        # fig1.layout.xaxis.title = "Charge vertical en tête de pieu [kN]"
+        # fig1.layout.yaxis.title = "Déplacement vertical en tête de pieu [mm]"
         st.plotly_chart(fig1, use_container_width=True)
 
 
