91d2eb8d7bac21b138c0170048e5545311ab20ad
Philippe VIVANT <philippe.vivant@gmail.com>
2026-02-17 21:06:10 +0100

    Update pile_app.py

--- pile_app.py
+++ pile_app.py
@@ -309,7 +309,7 @@ st.divider()
 
 st.subheader('Capacité résistante du pieu')
 
-colA, colB, colC = st.columns([5, 5, 4])
+colA, colB, colC = st.columns(3)
 with colA:
     st.markdown(
         f"""
